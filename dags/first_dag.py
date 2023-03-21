b3057ed3dbbf84f3dcb146e880e6841c545d3032
lucasbalponti <lucasbalponti@gmail.com>
2023-03-21 15:52:41 -0300

    Adicionando primeira dag e branch ubuntu

---
+++ dags/first_dag.py
@@ -0,0 +1,22 @@
+from airflow.models import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    'first_dag',
+    start_date=days_ago(1),
+    schedule_interval='@daily'
+) as dag:
+
+    tarefa_1 = EmptyOperator(task_id = 'tarefa_1')
+    tarefa_2 = EmptyOperator(task_id = 'tarefa_2')
+    tarefa_3 = EmptyOperator(task_id = 'tarefa_3')
+    tarefa_4 = BashOperator(
+        task_id = 'cria_pasta',
+        bash_command = 'mkdir -p "/home/lucasbalponti/Documents/airflowalura/dags/first_dag.py/pasta"'
+    )
+
+    tarefa_1 >> [tarefa_2, tarefa_3]
+    tarefa_3 >> tarefa_4
+
